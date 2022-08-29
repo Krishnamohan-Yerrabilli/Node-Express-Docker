a358a8bdd68fa695a1426b6deb1255c2bdb7531a
krishnamohan-yerrabilli <krishnamohan72452964@gmail.com>
2022-08-29 12:20:34 +0530

    etc

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const express = require("express");
 const app = express();
 
 app.get("/", (req, res) => {
-    res.send("<h1>Hey there this course is great</h1>");
+    res.send("<h1>Hey there bro this is an awesome course, I'm learning something here</h1>");
 })
 
 const port = process.env.PORT || 3000;
